bb00f3bdf7c4232ea2f92980baa928d06bca0eb2
inushtaev <inushtaev@samokat.ru>
2023-11-23 00:28:23 +0300

    implemented DebuggerController and custom logging

---
+++ src/main/java/ru/samokat/atlassian/jira/tutorials/sdkcondition/rest/DebuggerController.java
@@ -0,0 +1,24 @@
+package ru.samokat.atlassian.jira.tutorials.sdkcondition.rest;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/debugger_controller_class")
+@Slf4j
+public class DebuggerController {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("debugger_controller_method")
+    public String sayHello() {
+        log.debug("sayHello()");
+
+        return "Hello Jira REST World";
+    }
+
+}
+
